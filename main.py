f24e0e9f6b89f7b625bf2d3ea0fe314f28bf20e8
AceIsTired <162749835+AceIsTired@users.noreply.github.com>
2025-02-24 19:02:28 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,126 +1,83 @@
-# recitation-04
+"""
+CMPS 2200  Recitation 3.
+See recitation-03.md for details.
+"""
+import time
 
-from collections import defaultdict
+class BinaryNumber:
+    """ done """
+    def __init__(self, n):
+        self.decimal_val = n
+        self.binary_vec = list('{0:b}'.format(n))
 
-
-#### PART ONE ###
-
-def run_map_reduce(map_f, reduce_f, docs):
-    # done. do not change me.
-    """    
-    The main map reduce logic.
-    
-    Params:
-      map_f......the mapping function
-      reduce_f...the reduce function
-      docs.......list of input records
-    """
-    # 1. call map_f on each element of docs and flatten the results
-    # e.g., [('i', 1), ('am', 1), ('sam', 1), ('i', 1), ('am', 1), ('sam', 1), ('is', 1), ('ham', 1)]
-    pairs = flatten(list(map(map_f, docs)))
-    # 2. group all pairs by their key
-    # e.g., [('am', [1, 1]), ('ham', [1]), ('i', [1, 1]), ('is', [1]), ('sam', [1, 1])]
-    groups = collect(pairs)
-    # 3. reduce each group to the final answer
-    # e.g., [('am', 2), ('ham', 1), ('i', 2), ('is', 1), ('sam', 2)]
-    return [reduce_f(g) for g in groups]
-
-def word_count_map(doc):
-    """
-    Params:
-      doc....a string to be split into tokens. split on whitespace.
-    Returns:
-      a list of tuples of form (token, 1), where token is a whitespace delimited element of this string.
-      
-    E.g.
-    >>> word_count_map('i am sam i am')
-    [('i', 1), ('am', 1), ('sam', 1), ('i', 1), ('am', 1)]
-    """
-    ###TODO
-    
-    
+    def __repr__(self):
+        return('decimal=%d binary=%s' % (self.decimal_val, ''.join(self.binary_vec)))
 
 
-def word_count_reduce(group):
-    """
-    Params:
-      group...a tuple of the form (token, list_of_ones), indicating the number of times each word appears.
-    Returns:
-      tuple of form (token, int), where int is the number of times that token appears
-    E.g.
-    >>> word_count_reduce(['i', [1,1]])
-    ('i', 2)
-    
-    NOTE: you should use call the `reduce` function here.
-    """
-    ###TODO
-    
-    
+## Implement multiplication functions here. Note that you will have to
+## ensure that x, y are appropriately sized binary vectors for a
+## divide and conquer approach.
+
+def binary2int(binary_vec):
+    if len(binary_vec) == 0:
+        return BinaryNumber(0)
+    return BinaryNumber(int(''.join(binary_vec), 2))
+
+def split_number(vec):
+    return (binary2int(vec[:len(vec)//2]),
+            binary2int(vec[len(vec)//2:]))
+
+def bit_shift(number, n):
+    # append n 0s to this number's binary string
+    return binary2int(number.binary_vec + ['0'] * n)
+
+def pad(x,y):
+    # pad with leading 0 if x/y have different number of bits
+    # e.g., [1,0] vs [1]
+    if len(x) < len(y):
+        x = ['0'] * (len(y)-len(x)) + x
+    elif len(y) < len(x):
+        y = ['0'] * (len(x)-len(y)) + y
+    # pad with leading 0 if not even number of bits
+    if len(x) % 2 != 0:
+        x = ['0'] + x
+        y = ['0'] + y
+    return x,y
+
+def quadratic_multiply(x, y):
+    # this just converts the result from a BinaryNumber to a regular int
+    return _quadratic_multiply(x,y).decimal_val
+
+def _quadratic_multiply(x, y):
+    newx, newy = pad(x.binary_vec, y.binary_vec)
+
+    if x.decimal_val <= 1 and y.decimal_val <= 1:
+        return BinaryNumber(x.decimal_val * y.decimal_val)
+
+    xleft, xright = split_number(newx)
+    yleft, yright = split_number(newy)
+
+    j = bit_shift(_quadratic_multiply(xleft, yleft), len(newx))
+    k = _quadratic_multiply(xright, yright)
+
+    la = _quadratic_multiply(xleft, yright)
+    lb = _quadratic_multiply(xright, yleft)
+    l = bit_shift(BinaryNumber(la.decimal_val + lb.decimal_val), len(newx)//2)
+
+    result = BinaryNumber(j.decimal_val + l.decimal_val + k.decimal_val)
+    return result
+    ###
 
 
-def iterate(f, x, a):
-    # done. do not change me.
-    """
-    Params:
-      f.....function to apply
-      x.....return when a is empty
-      a.....input sequence
-    """
-    if len(a) == 0:
-        return x
-    else:
-        return iterate(f, f(x, a[0]), a[1:])
-    
-def flatten(sequences):
-    # done. do not change me.
-    return iterate(plus, [], sequences)
-
-def collect(pairs):
-    """
-    # done. do not change me.
-    Implements the collect function (see text Vol II Ch2)
-    E.g.:
-    >>> collect([('i', 1), ('am', 1), ('sam', 1), ('i', 1)])
-    [('am', [1]), ('i', [1, 1]), ('sam', [1])]    
-    """
-    result = defaultdict(list)
-    for pair in sorted(pairs):
-        result[pair[0]].append(pair[1])
-    return list(result.items())
 
 
-def plus(x, y):
-    # done. do not change me.
-    return x + y
+def test_quadratic_multiply(x, y, f):
+    start = time.time()
+    # multiply two numbers x, y using function f
+    finish = f(x, y)
+    return finish, (time.time() - start)*1000
 
-def reduce(f, id_, a):
-    # done. do not change me.
-    if len(a) == 0:
-        return id_
-    elif len(a) == 1:
-        return a[0]
-    else:
-        return f(reduce(f, id_, a[:len(a)//2]),
-                 reduce(f, id_, a[len(a)//2:]))
-    
-    
-    
-    
-### PART TWO ###
 
-def sentiment_map(doc,
-                  pos_terms=set(['good', 'great', 'awesome', 'sockdolager']),
-                  neg_terms=set(['bad', 'terrible', 'waste', 'carbuncle', 'corrupted'])):
-    """
-    Params:
-      doc.........a string to be split into tokens. split on whitespace.
-      pos_terms...a set of positive terms
-      neg_terms...a set of negative terms
-    Returns:
-      a list of tuples of form (positive, 1) or (negative, 1)      
-    E.g.
-    >>> sentiment_map('it was a terrible waste of time')
-    [('negative', 1), ('negative', 1)]
-    """
-    ###TODO
+
+
 
